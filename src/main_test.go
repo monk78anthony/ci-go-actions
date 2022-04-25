f57db7437478159e9a038e9ff2afba4f7ff35cd0
Ant Andrades <anthony.andrades@schneider-electric.com>
2022-04-25 20:33:15 +0100

    Update main_test.go

--- src/main_test.go
+++ src/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    expected := "Hello World!"
+    expected := "Listening on port 5050..."
     if got := print(); got != expected {
         t.Errorf("print() = %q, want %q", got, expected)
     }
